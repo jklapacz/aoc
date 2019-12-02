e9a0e21e8bc3c69844157a9091701e8b548165dc
Jakub Klapacz <jklapacz@pindrop.com>
2019-12-02 10:28:42 -0500

    Solve day 2 part 1

--- 02/main.go
+++ 02/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/jklapacz/aoc"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	OpcodeAdd = 1
+	OpcodeMultiply = 2
+	OpcodeErr = 99
 )
 
 func main() {
@@ -18,7 +27,66 @@ func main() {
 	solve(puzzle)
 }
 
+type command = *bytes.Buffer
+
+
 func solve(p *aoc.Puzzle) {
+	for p.Instance.Scan() {
+		cmd := bytes.NewBufferString(p.Instance.Text())
+		execute(cmd)
+	}
 	fmt.Println("Solving!")
-	p.DumpContents()
 }
+
+func execute(cmd command) {
+	rawOperations := strings.Split(cmd.String(), ",")
+	operations := make([]int, len(rawOperations))
+	for idx, op := range rawOperations {
+		parsedVal, err := strconv.ParseInt(op, 10, 32)
+		if err != nil {
+			log.Fatal("Could not parse opcode")
+		}
+		operations[idx] = int(parsedVal)
+	}
+	startingIdx := 0
+	offsetIncrement := 4
+	for curIdx := startingIdx; curIdx + offsetIncrement < len(operations); curIdx += offsetIncrement {
+		fmt.Printf("Next iteration, opcode index: %v \n", curIdx)
+		performOperation(operations, curIdx)
+	}
+	fmt.Println("Output", operations)
+}
+
+func performOperation(opcodes []int, opcodeBit int) {
+	switch opcodes[opcodeBit] {
+	case OpcodeAdd:
+		performAddOp(opcodes, opcodeBit)
+	case OpcodeMultiply:
+		performMultOp(opcodes, opcodeBit)
+	case OpcodeErr:
+		fmt.Println("Error opcode found")
+	default:
+		log.Fatal("Unexpected error")
+	}
+}
+
+func performAddOp(opcodes []int, operationOffset int) {
+	IdxAddFirst := opcodes[operationOffset + 1]
+	IdxAddSecond := opcodes[operationOffset + 2]
+	IdxTarget := opcodes[operationOffset + 3]
+	fmt.Printf("Operating on indicies %v, %v and %v\n", IdxAddFirst, IdxAddSecond, IdxTarget)
+	fmt.Printf("Value at %v before: %v \n", IdxTarget, opcodes[IdxTarget])
+	opcodes[IdxTarget] = opcodes[IdxAddFirst] + opcodes[IdxAddSecond]
+	fmt.Printf("Value at %v after: %v \n",IdxTarget, opcodes[IdxTarget])
+}
+
+func performMultOp(opcodes []int, operationOffset int) {
+	IdxFirst := opcodes[operationOffset + 1]
+	IdxSecond := opcodes[operationOffset + 2]
+	IdxTarget := opcodes[operationOffset + 3]
+	fmt.Printf("Operating on indicies %v, %v and %v\n", IdxFirst, IdxSecond, IdxTarget)
+	fmt.Printf("Value at %v before: %v \n", IdxTarget, opcodes[IdxTarget])
+	opcodes[IdxTarget] = opcodes[IdxFirst] * opcodes[IdxSecond]
+	fmt.Printf("Value at %v after: %v \n",IdxTarget, opcodes[IdxTarget])
+}
+
