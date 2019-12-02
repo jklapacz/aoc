b5d24d5e408d55dc6e1e92179324b0bc87506b8e
Jakub Klapacz <jklapacz@pindrop.com>
2019-12-02 09:42:00 -0500

    Puzzle extracted into common utility

--- 01/main.go
+++ 01/main.go
@@ -1,65 +1,37 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"github.com/jklapacz/aoc"
 	"log"
 	"os"
 	"strconv"
 )
 
 type Puzzle struct {
-	filename string
-	instance *PuzzleInstance
+	*aoc.Puzzle
 }
-
-type PuzzleInstance = bufio.Scanner
 
 func main() {
 	fileArg := os.Args[1:]
 	if len(fileArg) < 1 {
 		panic("No argument given!")
 	}
-	puzzle := &Puzzle{filename: fileArg[0]}
-	cleanup := puzzle.setupPuzzle()
+	puzzle := &Puzzle{&aoc.Puzzle{Filename: fileArg[0]}}
+	cleanup := puzzle.SetupPuzzle()
 	defer cleanup()
 
 	puzzle.Solve()
 }
 
-func (p *Puzzle) setupPuzzle() func() {
-	if p.filename == "" {
-		panic("no file given!")
-	}
-	file, err := os.Open(p.filename)
-	if err != nil {
-		panic(fmt.Sprintf("could not open file!: %v", err))
-	}
-	p.instance = instantiatePuzzle(file)
-	return func() { file.Close() }
-}
-
-func instantiatePuzzle(file *os.File) *PuzzleInstance {
-	return bufio.NewScanner(file)
-}
-
-// mostly for debugging, iterate through the file
-func (p *Puzzle) dumpContents() {
-	if p.instance == nil {
-		log.Fatal("puzzle is not instantiated!")
-	}
-	for p.instance.Scan() {
-		fmt.Println(p.instance.Text())
-	}
-}
 
 type moduleWeight = int
 type fuelCost = int
 
 func (p *Puzzle) Solve() {
 	fuelSum := 0
-	for p.instance.Scan() {
-		currentInput := p.instance.Text()
+	for p.Instance.Scan() {
+		currentInput := p.Instance.Text()
 		weight := turnInputIntoWeight(currentInput)
 		fuelSum += calculateFuelCostCost(weight)
 	}
---
+++ puzzle.go
@@ -0,0 +1,46 @@
+package aoc
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Puzzle struct {
+	Filename string
+	Instance *Instance
+}
+
+type Instance = bufio.Scanner
+
+func Hello() {
+	fmt.Println("Hello")
+}
+
+func (p *Puzzle) SetupPuzzle() func() {
+	if p.Filename == "" {
+		panic("no file given!")
+	}
+	file, err := os.Open(p.Filename)
+	if err != nil {
+		panic(fmt.Sprintf("could not open file!: %v", err))
+	}
+	p.Instance = instantiatePuzzle(file)
+	return func() { file.Close() }
+}
+
+func instantiatePuzzle(file *os.File) *Instance {
+	return bufio.NewScanner(file)
+}
+
+// mostly for debugging, iterate through the file
+func (p *Puzzle) dumpContents() {
+	if p.Instance == nil {
+		log.Fatal("puzzle is not instantiated!")
+	}
+	for p.Instance.Scan() {
+		fmt.Println(p.Instance.Text())
+	}
+}
+
